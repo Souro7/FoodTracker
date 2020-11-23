c021e51ad51966c58474cc324027969ad54b4a10
Souro7 <sarkarsourobrata@gmail.con>
2020-11-23 09:51:32 +0530

    Add IBOutlets and IBAction

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default text"
+    }
+    
 
 
 }

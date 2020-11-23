32fa9ae1a32de0b5954a312f2293158bf6d8f3d4
Sourobrata Sarkar <sourobrata.s@mobstac.com>
2020-11-23 09:07:47 +0530

    Initial commit: Add UI elements

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Sourobrata Sarkar on 23/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
